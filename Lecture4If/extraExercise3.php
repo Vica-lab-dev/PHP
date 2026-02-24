4d2a6a94474a7e7425e9899cef6586ed13f7284c
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-24 23:26:07 +0100

    feat: determine if $number is positive, negative, or zero using if/else

---
+++ Lecture4If/extraExercise3.php
@@ -0,0 +1,16 @@
+<?php
+
+$number = -1;
+
+if($number < 0)
+{
+    echo ("Number is negative");
+}
+elseif($number > 0)
+{
+    echo ("Number is positive");
+}
+else
+{
+    echo ("Number is zero");
+}
