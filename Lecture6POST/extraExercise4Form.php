c0dba3fb85c1a87280b0e942f0d5c8c58bcf3d03
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-25 00:05:47 +0100

    feat: add POST form and display month name based on $month using switch

---
+++ Lecture6POST/extraExercise4Form.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Document</title>
+    </head>
+    <body>
+    <form action="extraExercise4.php" method="POST">
+        <input type="number" name="month">
+        <button>Check month</button>
+    </form>
+
+    </body>
+</html>
