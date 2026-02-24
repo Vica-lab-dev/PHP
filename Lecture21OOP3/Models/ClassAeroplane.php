7aba3a2218bf830e9ee0178fee133e9ebcc28ccd
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-24 22:26:48 +0100

    feat: add ClassAeroplane class

---
+++ Lecture21OOP3/Models/ClassAeroplane.php
@@ -0,0 +1,10 @@
+<?php
+
+    require_once "ClassCar.php";
+    class Aeroplane extends Car
+    {
+        public $propulsion; //Pogon
+        public $numberOfWheels;
+        public $numberOfSeats;
+        public $parachute;
+    }
