6c70fd50b9901197de2b2adfc0ce1b10e973b81d
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-24 22:28:04 +0100

    feat: add ClassBus class

---
+++ Lecture21OOP3/Models/ClassBus.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once "ClassCar.php";
+    class Bus extends Car
+    {
+        public $numberOfSeats;
+        public $numberOfDoors;
+        public $doubleDecker; //Dvospratni
+    }
