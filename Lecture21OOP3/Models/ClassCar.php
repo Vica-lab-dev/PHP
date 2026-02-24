1de7bda723a5a37ffb0929f3c1da2343ef573ab5
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-24 22:27:56 +0100

    feat: add ClassCar class

---
+++ Lecture21OOP3/Models/ClassCar.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Car
+    {
+        public $weight;
+        public $height;
+        public $type;
+        public $brand; //Marka
+        public $model;
+        public $color;
+    }
+
